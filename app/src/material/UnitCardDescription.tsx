babda7e1b01c867c82b69d9f6122dafc7b1b131d
Ocepow <ocepow@outlook.fr>
2024-09-08 16:29:26 +0200

    backimage

---
+++ app/src/material/UnitCardDescription.tsx
@@ -0,0 +1,14 @@
+import { CardDescription } from "@gamepark/react-game";
+import pt_integral_cards_back_fr from '../images/units/pt_integral_cards_back_fr.jpg'
+
+export class RegionCardDescription extends CardDescription {
+    height = 7
+    width = 7
+    borderRadius = 0.5
+
+backImage = pt_integral_cards_back_fr.jpg
+
+
+
+
+}
