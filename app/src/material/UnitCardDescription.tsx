e9f984a3d6399f6f02cb720200622fa03be5f8d2
Patrick Beaume <p.beaume@gmail.com>
2024-09-08 17:06:08 +0200

    add images

--- app/src/material/UnitCardDescription.tsx
+++ app/src/material/UnitCardDescription.tsx
@@ -1,23 +1,101 @@
-import { CardDescription } from "@gamepark/react-game";
-import pt_integral_cards_back_fr from '../images/units/pt_integral_cards_back_fr.jpg'
-import pt_integral_cards_front_fr27 from '../images/units/pt_integral_cards_front_fr27.jpg'
-
 import { Unit } from '@gamepark/paper-tales/material/Unit'
+import { CardDescription } from '@gamepark/react-game'
+import Back from '../images/units/pt_integral_cards_back_fr.jpg'
+import Commander from '../images/units/en/Commander.jpg'
+import Adventurer from '../images/units/en/Adventurer.jpg'
+import Archer from '../images/units/en/Archer.jpg'
+import Blacksmith from '../images/units/en/Blacksmith.jpg'
+import CaveSpirit from '../images/units/en/CaveSpirit.jpg'
+import Cerberus from '../images/units/en/Cerberus.jpg'
+import Colossus from '../images/units/en/Colossus.jpg'
+import Cook from '../images/units/en/Cook.jpg'
+import Demon from '../images/units/en/Demon.jpg'
+import Dragon from '../images/units/en/Dragon.jpg'
+import Farmer from '../images/units/en/Farmer.jpg'
+import ForestChild from '../images/units/en/ForestChild.jpg'
+import ForestSpirit from '../images/units/en/ForestSpirit.jpg'
+import GiantSnake from '../images/units/en/GiantSnake.jpg'
+import Golem from '../images/units/en/Golem.jpg'
+import GrandArchitect from '../images/units/en/GrandArchitect.jpg'
+import Hunter from '../images/units/en/Hunter.jpg'
+import Knight from '../images/units/en/Knight.jpg'
+import kraken from '../images/units/en/kraken.jpg'
+import Landlord from '../images/units/en/Landlord.jpg'
+import Leviathan from '../images/units/en/Leviathan.jpg'
+import Manticore from '../images/units/en/Manticore.jpg'
+import MasterAlchemist from '../images/units/en/MasterAlchemist.jpg'
+import Militiaman from '../images/units/en/Militiaman.jpg'
+import Miner from '../images/units/en/Miner.jpg'
+import Monkey from '../images/units/en/Monkey.jpg'
+import MysticalHealer from '../images/units/en/MysticalHealer.jpg'
+import Necromancer from '../images/units/en/Necromancer.jpg'
+import Ogre from '../images/units/en/Ogre.jpg'
+import PalmReader from '../images/units/en/PalmReader.jpg'
+import Peddler from '../images/units/en/Peddler.jpg'
+import RelicOfWisdom from '../images/units/en/RelicOfWisdom.jpg'
+import Rogue from '../images/units/en/Rogue.jpg'
+import Salamander from '../images/units/en/Salamander.jpg'
+import Sculptor from '../images/units/en/Sculptor.jpg'
+import Shapeshifter from '../images/units/en/Shapeshifter.jpg'
+import Strategist from '../images/units/en/Strategist.jpg'
+import TimeMaster from '../images/units/en/TimeMaster.jpg'
+import Treefolk from '../images/units/en/Treefolk.jpg'
+import Veteran from '../images/units/en/Veteran.jpg'
+import Woodcutter from '../images/units/en/Woodcutter.jpg'
+import WoodMerchant from '../images/units/en/WoodMerchant.jpg'
 
 export class RegionCardDescription extends CardDescription {
-    height = 7
-    width = 7
-    borderRadius = 0.5
+  height = 7
+  width = 7
+  borderRadius = 0.5
 
-backImage = pt_integral_cards_back_fr
+  backImage = Back
 
-//Image en FR.
-
-images = {
-    [Unit.Commander] : pt_integral_cards_front_fr27,
-
-}
-
+//Image en EN.
+  images = {
+    [Unit.Commander]: Commander,
+    [Unit.Adventurer]: Adventurer,
+    [Unit.Archer]: Archer,
+    [Unit.Blacksmith]: Blacksmith,
+    [Unit.CaveSpirit]: CaveSpirit,
+    [Unit.Cerberus]: Cerberus,
+    [Unit.Colossus]: Colossus,
+    [Unit.Cook]: Cook,
+    [Unit.Demon]: Demon,
+    [Unit.Dragon]: Dragon,
+    [Unit.Farmer]: Farmer,
+    [Unit.ForestChild]: ForestChild,
+    [Unit.ForestSpirit]: ForestSpirit,
+    [Unit.GiantSnake]: GiantSnake,
+    [Unit.Golem]: Golem,
+    [Unit.GrandArchitect]: GrandArchitect,
+    [Unit.Hunter]: Hunter,
+    [Unit.Knight]: Knight,
+    [Unit.Kraken]: kraken,
+    [Unit.LandLord]: Landlord,
+    [Unit.Leviathan]: Leviathan,
+    [Unit.Manticore]: Manticore,
+    [Unit.MasterAlchemist]: MasterAlchemist,
+    [Unit.Militiaman]: Militiaman,
+    [Unit.Miner]: Miner,
+    [Unit.Monkey]: Monkey,
+    [Unit.MysticalHealer]: MysticalHealer,
+    [Unit.Necromancer]: Necromancer,
+    [Unit.Ogre]: Ogre,
+    [Unit.PalmReader]: PalmReader,
+    [Unit.Peddler]: Peddler,
+    [Unit.RelicOfWisdom]: RelicOfWisdom,
+    [Unit.Rogue]: Rogue,
+    [Unit.Salamander]: Salamander,
+    [Unit.Sculptor]: Sculptor,
+    [Unit.Shapeshifter]: Shapeshifter,
+    [Unit.Strategist]: Strategist,
+    [Unit.TimeMaster]: TimeMaster,
+    [Unit.Treefolk]: Treefolk,
+    [Unit.Veteran]: Veteran,
+    [Unit.Woodcutter]: Woodcutter,
+    [Unit.WoodMerchant]: WoodMerchant,
+  }
 
 
 }
