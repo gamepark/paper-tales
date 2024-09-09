aaed6d1c9b216ad7f5fb875428eb7ca05f4edec5
Ocepow <ocepow@outlook.fr>
2024-09-09 12:03:17 +0200

    decklocator

--- app/src/locators/Locators.tsx
+++ app/src/locators/Locators.tsx
@@ -2,5 +2,10 @@ import { LocationType } from '@gamepark/paper-tales/material/LocationType'
 import { MaterialType } from '@gamepark/paper-tales/material/MaterialType'
 import { PlayerColor } from '@gamepark/paper-tales/PlayerColor'
 import { Locator } from '@gamepark/react-game'
+import { PaperTalesDeckLocator } from './DeckLocator'
 
-export const Locators: Partial<Record<LocationType, Locator<PlayerColor, MaterialType, LocationType>>> = {}
+export const Locators: Partial<Record<LocationType, Locator<PlayerColor, MaterialType, LocationType>>> = {
+
+    [LocationType.Deck]: new PaperTalesDeckLocator(),
+}
+
