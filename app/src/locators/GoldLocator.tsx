26bb86cc47330fd8c876073c397099c66dc2febe
Ocepow <ocepow@outlook.fr>
2024-09-09 12:26:05 +0200

    GoldLocator

---
+++ app/src/locators/GoldLocator.tsx
@@ -0,0 +1,7 @@
+import { FlexLocator } from '@gamepark/react-game'
+
+
+export class PaperTalesGoldLocator extends FlexLocator {
+  coordinates = { x: -30, y: -30, z: 0 }
+  delta = { x: -0.05, y: -0.05, z: 0.1 }
+}
