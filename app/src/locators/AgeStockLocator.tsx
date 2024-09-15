7bddddba6f263b8b23e07d6f0933c964e3ae0dca
Ocepow <ocepow@outlook.fr>
2024-09-15 11:07:41 +0200

    fix

---
+++ app/src/locators/AgeStockLocator.tsx
@@ -0,0 +1,6 @@
+import { FlexLocator } from '@gamepark/react-game'
+
+export class AgeStockLocator extends FlexLocator {
+  coordinates = { x: -40, y: -30, z: 0 }
+  delta = { x: -0.05, y: -0.05, z: 0.1 }
+}
