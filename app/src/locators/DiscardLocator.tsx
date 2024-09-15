b2ea87d849ab3c83f0aae8e84a69e7c1dde049f3
Ocepow <ocepow@outlook.fr>
2024-09-15 10:42:56 +0200

    fix name

--- app/src/locators/DiscardLocator.tsx
+++ app/src/locators/DiscardLocator.tsx
@@ -1,6 +1,6 @@
 import { DeckLocator } from '@gamepark/react-game'
 
-export class PaperTalesDiscardLocator extends DeckLocator {
-  coordinates = { x: -30, y: -20, z: 0 }
+export class DiscardLocator extends DeckLocator {
+  coordinates = { x: -40, y: -10, z: 0 }
   delta = { x: -0.05, y: -0.05, z: 0.1 }
 }
--- app/src/locators/Locators.tsx
+++ app/src/locators/Locators.tsx
@@ -3,14 +3,14 @@ import { MaterialType } from '@gamepark/paper-tales/material/MaterialType'
 import { PlayerColor } from '@gamepark/paper-tales/PlayerColor'
 import { Locator } from '@gamepark/react-game'
 import { UnitDeckLocator } from './DeckLocator'
-import { PaperTalesGoldLocator } from './GoldLocator'
-import { PaperTalesDiscardLocator } from './DiscardLocator'
+import { GoldLocator } from './GoldLocator'
+import { DiscardLocator } from './DiscardLocator'
 
 export const Locators: Partial<Record<LocationType, Locator<PlayerColor, MaterialType, LocationType>>> = {
 
     [LocationType.Deck]: new UnitDeckLocator(),
-    [LocationType.GoldStock]: new PaperTalesGoldLocator(),
-    [LocationType.Discard]: new PaperTalesDiscardLocator(),
+    [LocationType.GoldStock]: new GoldLocator(),
+    [LocationType.Discard]: new DiscardLocator(),
 
 }
 
