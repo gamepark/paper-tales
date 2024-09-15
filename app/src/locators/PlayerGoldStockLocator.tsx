24d720c561762608040f43696deebf3ae344bf59
Ocepow <ocepow@outlook.fr>
2024-09-15 11:13:09 +0200

    fix

--- app/src/locators/PlayerGoldStockLocator.tsx
+++ app/src/locators/PlayerGoldStockLocator.tsx
@@ -1,6 +1,6 @@
 import { FlexLocator } from '@gamepark/react-game'
 
-export class GoldStockLocator extends FlexLocator {
+export class PlayerGoldStockLocator extends FlexLocator {
   coordinates = { x: -30, y: -30, z: 0 }
   delta = { x: -0.05, y: -0.05, z: 0.1 }
 }
