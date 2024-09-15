40f06117b7588e3632e8445cf5b524fc0ca74380
Ocepow <ocepow@outlook.fr>
2024-09-15 10:58:30 +0200

    name locator

--- app/src/locators/Locators.tsx
+++ app/src/locators/Locators.tsx
@@ -3,14 +3,14 @@ import { MaterialType } from '@gamepark/paper-tales/material/MaterialType'
 import { PlayerColor } from '@gamepark/paper-tales/PlayerColor'
 import { Locator } from '@gamepark/react-game'
 import { UnitDeckLocator } from './DeckLocator'
-import { GoldLocator } from './GoldLocator'
+import { GoldStockLocator } from './GoldStockLocator'
 import { DiscardLocator } from './DiscardLocator'
 import { PlayerDraftHandLocator } from './PlayerHandLocator'
 
 export const Locators: Partial<Record<LocationType, Locator<PlayerColor, MaterialType, LocationType>>> = {
 
     [LocationType.Deck]: new UnitDeckLocator(),
-    [LocationType.GoldStock]: new GoldLocator(),
+    [LocationType.GoldStock]: new GoldStockLocator(),
     [LocationType.Discard]: new DiscardLocator(),
     [LocationType.PlayerDraftHand]: new PlayerDraftHandLocator()
 
