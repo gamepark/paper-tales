9c7d0a469cc2ca95d5a4f4b4b8de2ad012c415cc
Ocepow <ocepow@outlook.fr>
2024-09-30 09:58:33 +0200

    bulding card description

---
+++ app/src/material/BuildingCardDescription.tsx
@@ -0,0 +1,28 @@
+import { Building } from '@gamepark/paper-tales/material/Building'
+import { CardDescription } from '@gamepark/react-game'
+import Mine from '../images/buildings/en/level1/MineL1.jpg'
+import Barracks from '../images/buildings/en/level1/BarracksL1.jpg'
+import Tavern from '../images/buildings/en/level1/TavernL1.jpg'
+import Temple from '../images/buildings/en/level1/TempleL1.jpg'
+import Town from '../images/buildings/en/level1/TownL1.jpg'
+
+
+export class BuildingCardDescription extends CardDescription {
+    height = 14
+    width = 10
+    borderRadius = 0.5
+  
+  
+  //Image en EN.
+    images = {
+      [Building.Mine] : Mine,
+      [Building.Barracks] : Barracks,
+      [Building.Tavern] : Tavern,
+      [Building.Temple] : Temple,
+      [Building.Town] : Town,
+
+    }
+  
+  }
+  
+  export const buildingCardDescription = new BuildingCardDescription()
--- app/src/material/Material.tsx
+++ app/src/material/Material.tsx
@@ -3,11 +3,13 @@ import { MaterialDescription } from '@gamepark/react-game'
 import { unitCardDescription } from './UnitCardDescription'
 import { goldCoinDescription } from './goldCoinDescription'
 import { ageTokenDescription } from './ageTokenDescription'
+import { buildingCardDescription } from './BuildingCardDescription'
 
 export const Material: Partial<Record<MaterialType, MaterialDescription>> = {
 
     [MaterialType.Unit]: unitCardDescription,
     [MaterialType.Gold]: goldCoinDescription,
     [MaterialType.Age]: ageTokenDescription,
+    [MaterialType.Building]: buildingCardDescription,
 
 }
