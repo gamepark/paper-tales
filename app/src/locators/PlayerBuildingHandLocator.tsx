f0499faf9f0adab5797c33df4f3fe231acc32ac5
Ocepow <ocepow@outlook.fr>
2024-10-01 09:51:38 +0200

    work player bulding hand locator

--- app/src/locators/PlayerBuildingHandLocator.tsx
+++ app/src/locators/PlayerBuildingHandLocator.tsx
@@ -4,6 +4,10 @@ import { HandLocator } from "@gamepark/react-game";
 
 export class PlayerBuildingHandLocator extends HandLocator {
 
+    coordinates = { x: -50, y: -25, z: 0 }
+    radius = 2
+  
+
 }
 
 
