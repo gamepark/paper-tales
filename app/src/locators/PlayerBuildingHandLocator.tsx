8c1dcf525f28ecd8a5e292a6d8533d5275448742
Ocepow <ocepow@outlook.fr>
2024-10-02 16:31:48 +0200

    placement of bulding

--- app/src/locators/PlayerBuildingHandLocator.tsx
+++ app/src/locators/PlayerBuildingHandLocator.tsx
@@ -1,4 +1,7 @@
-import { DropAreaDescription, HandLocator } from "@gamepark/react-game";
+import { LocationType } from "@gamepark/paper-tales/material/LocationType";
+import { MaterialType } from "@gamepark/paper-tales/material/MaterialType";
+import { DropAreaDescription, HandLocator, ItemContext } from "@gamepark/react-game";
+import { MaterialItem } from "@gamepark/rules-api";
 
 
 
@@ -6,9 +9,18 @@ export class PlayerBuildingHandLocator extends HandLocator {
 
     locationDescription = new DropAreaDescription({ width: 20, height: 8, borderRadius: 0.4 })
 
-    coordinates = { x: -50, y: 5, z: 0 }
+    coordinates = { x: -40, y: 5, z: 0 }
     radius = 2
   
+    getItemIndex(item: MaterialItem, context: ItemContext): number {
+        if (item.location.player === context.player) {
+          const cards = context.rules.material(MaterialType.Building).location(LocationType.PlayerBuildingHand).player(context.player).getItems().map(item => item.id)
+          cards.sort((a, b) => a - b)
+          return cards.indexOf(item.id)
+        } else {
+          return super.getItemIndex(item, context)
+        }
+      }
 
 }
 
