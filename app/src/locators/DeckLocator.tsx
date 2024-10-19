44d61465e0f61bd7e1cad3a928b2dfc9c1eadea1
Ocepow <ocepow@outlook.fr>
2024-10-19 14:55:17 +0200

    discoard locator and deck locator

--- app/src/locators/DeckLocator.tsx
+++ app/src/locators/DeckLocator.tsx
@@ -1,6 +1,6 @@
 import { DeckLocator } from '@gamepark/react-game'
 
 export class UnitDeckLocator extends DeckLocator {
-  coordinates = { x: -5, y: -20, z: 0 }
+  coordinates = { x: -6, y: -30, z: 0 }
   delta = { x: -0.05, y: -0.05, z: 0.1 }
 }
