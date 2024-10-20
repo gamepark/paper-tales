2acad27f1d6d5b9d847a0efafe72a48689f05d9d
Ocepow <ocepow@outlook.fr>
2024-10-20 13:09:41 +0200

    draft header

--- app/src/headers/DraftHeader.tsx
+++ app/src/headers/DraftHeader.tsx
@@ -1,8 +1,20 @@
 /** @jsxImportSource @emotion/react */
 
+import { useLegalMoves } from "@gamepark/react-game"
+import { useTranslation } from "react-i18next"
 
 export const DraftHeader = () => {
-    return <>
+  const { t } = useTranslation()
+  const legalMoves = useLegalMoves()
+  console.log(legalMoves)
+
+  return <>
+
+    {legalMoves.length !== 0
+      ? <div>{t('choose.card')}</div>
+      : <div>{t('wait.players')}</div>
+    }
 
   </>
+
 }
