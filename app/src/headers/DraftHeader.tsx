2d6a11525550f35316906a7a6ffaabacdec7e07e
Ocepow <ocepow@outlook.fr>
2024-10-20 13:13:50 +0200

    fix

--- app/src/headers/DraftHeader.tsx
+++ app/src/headers/DraftHeader.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from "react-i18next"
 export const DraftHeader = () => {
   const { t } = useTranslation()
   const legalMoves = useLegalMoves()
-  console.log(legalMoves)
+
 
   return <>
 
