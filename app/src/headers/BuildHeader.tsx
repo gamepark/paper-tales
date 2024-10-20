f0aebba3766fac83260ac794c6a17c4486bc6b17
Ocepow <ocepow@outlook.fr>
2024-10-20 13:00:41 +0200

    fix

--- app/src/headers/BuildHeader.tsx
+++ app/src/headers/BuildHeader.tsx
@@ -1,21 +1,24 @@
 /** @jsxImportSource @emotion/react */
 
 import { PlayMoveButton, useLegalMoves } from "@gamepark/react-game"
+import { useTranslation } from "react-i18next"
 
 
 export const BuildHeader = () => {
 
   const legalMoves = useLegalMoves()
   const endMove = legalMoves.find(move => move.type === 2)
-  console.log(legalMoves)
+  const { t } = useTranslation()
 
 
   return <>
+
+
     {endMove !== undefined
       ? <PlayMoveButton move={endMove}>
-        <div> end.turn.build</div>
+        <div> {t('end.turn.build')}</div>
       </PlayMoveButton>
-      : <div> wait.turn.build  </div>
+      : <div> {t('wait.turn.build')}  </div>
     }
 
 
