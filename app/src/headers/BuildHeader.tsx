87fa0891f7daf0fa012bd80f62af37e9b6a59ef0
Ocepow <ocepow@outlook.fr>
2024-10-20 12:30:58 +0200

    fix build header

--- app/src/headers/BuildHeader.tsx
+++ app/src/headers/BuildHeader.tsx
@@ -6,14 +6,14 @@ import { PlayMoveButton, useLegalMoves } from "@gamepark/react-game"
 export const BuildHeader = () => {
 
   const legalMoves = useLegalMoves()
-  const endMove = legalMoves.find(move => move.type > 0 )
+  const endMove = legalMoves.find(move => move.type === 2)
   console.log(legalMoves)
 
 
   return <>
 
     <PlayMoveButton move={endMove}>
-      <div> coucou</div>
+      <div> end.turn.build</div>
     </PlayMoveButton>
 
 
