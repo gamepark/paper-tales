3963e81d73dd01cd869214b1f25341125f4e20d2
Ocepow <ocepow@outlook.fr>
2024-10-20 11:04:56 +0200

    build turn end header

---
+++ app/src/headers/BuildHeader.tsx
@@ -0,0 +1,23 @@
+/** @jsxImportSource @emotion/react */
+
+import { PlayMoveButton, useLegalMoves } from "@gamepark/react-game"
+
+
+export const BuildHeader = () => {
+
+  const legalMoves = useLegalMoves()
+  const endMove = legalMoves.find(move => move.type > 0 )
+  console.log(legalMoves)
+
+
+  return <>
+
+    <PlayMoveButton move={endMove}>
+      <div> coucou</div>
+    </PlayMoveButton>
+
+
+  </>
+}
+
+
