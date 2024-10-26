df23dd92abf55a34af588b7bc37401765f74677b
Ocepow <ocepow@outlook.fr>
2024-10-26 11:11:21 +0200

    plateau central score

--- app/src/locators/ScoreBoardLocator.tsx
+++ app/src/locators/ScoreBoardLocator.tsx
@@ -1,7 +1,7 @@
 import { Locator } from '@gamepark/react-game'
 
 export class ScoreBoardLocator extends Locator {
-  coordinates = { x: 0, y: -10, z: 0 }
+  coordinates = { x: 0, y: -5, z: 0 }
 }
 
 export const scoreBoardLocator = new ScoreBoardLocator()
