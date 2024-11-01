a0db1893a192e33e429de3d1f8e58b407603e6a6
Ocepow <ocepow@outlook.fr>
2024-11-01 12:04:58 +0100

    pannel score

--- app/src/panels/PaperTalesPlayerPanel.tsx
+++ app/src/panels/PaperTalesPlayerPanel.tsx
@@ -2,9 +2,14 @@
 import { css } from '@emotion/react'
 import { PaperTalesRules } from '@gamepark/paper-tales/PaperTalesRules'
 import { ScoreHelper } from '@gamepark/paper-tales/rules/helpers/ScoreHelper'
+import { ResourcesHelper } from '@gamepark/paper-tales/rules/helpers/ResourcesHelper'
 import { Player } from '@gamepark/react-client'
 import { CounterProps, StyledPlayerPanel, useFocusContext, useRules } from '@gamepark/react-game'
 import { FC, HTMLAttributes, useCallback, useMemo } from 'react'
+import gold from '../images/tokens/Gold1.jpg'
+import wood from '../images/ressources/ressources_bois.png'
+import food from '../images/ressources/ressources_viande.png'
+import diamond from '../images/ressources/ressources_minerai.png'
 
 type PaperTalesPlayerPanelProps = {
   player: Player,
@@ -15,6 +20,8 @@ export const PaperTalesPlayerPanel: FC<PaperTalesPlayerPanelProps> = (props) => 
   const { player, index, ...rest } = props
   const rules = useRules<PaperTalesRules>()!
   const scoreHelper = useMemo(() => new ScoreHelper(rules.game, player.id), [rules.game, player.id])
+  const ressourcesHelper = useMemo(() => new ResourcesHelper(rules.game, player.id), [rules.game, player.id])
+
 
   const { setFocus } = useFocusContext()
   const isBottomPlayers = rules.players.length === 5 ? (index === 0 || index === 4) : (rules.players.length === 4 ? (index === 0 || index === 3) : index === 0)
@@ -35,7 +42,25 @@ export const PaperTalesPlayerPanel: FC<PaperTalesPlayerPanelProps> = (props) => 
   const counters: CounterProps[] = [{
     image: '',
     value: scoreHelper.getScore(player.id)
-  }]
+  },
+  {
+    image: wood,
+    value: ressourcesHelper.getPlayerOneTypeResource(player.id, 1) 
+  },
+  {
+    image: food,
+    value: ressourcesHelper.getPlayerOneTypeResource(player.id, 2) 
+  },
+  {
+    image: diamond,
+    value: ressourcesHelper.getPlayerOneTypeResource(player.id, 3) 
+  },
+
+  {
+    image: gold, 
+    value: ressourcesHelper.getPlayerOneTypeResource(player.id, 4) 
+  }
+]
 
   return (
     <StyledPlayerPanel
@@ -44,7 +69,7 @@ export const PaperTalesPlayerPanel: FC<PaperTalesPlayerPanelProps> = (props) => 
       player={player}
       counters={counters}
       backgroundImage={panelBackgrounds[player.id]}
-      countersPerLine={2}
+      countersPerLine={3}
       css={canClick}
       {...rest}
     />
