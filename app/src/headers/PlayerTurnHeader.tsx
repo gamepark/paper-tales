aeef403cdd564d4a440b8128e0e1813e698148ea
Ocepow <ocepow@outlook.fr>
2025-02-24 09:55:50 +0100

    translaation playerturnheader

--- app/src/headers/PlayerTurnHeader.tsx
+++ app/src/headers/PlayerTurnHeader.tsx
@@ -1,18 +1,20 @@
 /** @jsxImportSource @emotion/react */
 
 import { PlayMoveButton, useLegalMoves } from "@gamepark/react-game"
+import { useTranslation } from "react-i18next"
 
 
 export const PlayerTurnHeader = () => {
 
   const legalMoves = useLegalMoves()
   const endMove = legalMoves.find(move => move.type === 2)
+  const { t } = useTranslation()
 
 
   return <>
 
     <PlayMoveButton move={endMove}>
-      <div> end.turn.player</div>
+    <div>{t('end.turn.player')}</div>
     </PlayMoveButton>
 
 
